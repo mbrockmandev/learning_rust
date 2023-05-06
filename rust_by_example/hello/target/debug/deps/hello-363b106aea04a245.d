3be22d4c3e663a3b15cc65514c8748e6c8136b5a
Michael Brockman <mbrockmandev@gmail.com>
2023-05-06 14:35:00 -0400

    add practice problems from online book

---
+++ rust_by_example/hello/target/debug/deps/hello-363b106aea04a245.d
@@ -0,0 +1,5 @@
+/Users/mike/Developer/rust/learning_rust/rust_by_example/hello/target/debug/deps/hello-363b106aea04a245.rmeta: src/main.rs
+
+/Users/mike/Developer/rust/learning_rust/rust_by_example/hello/target/debug/deps/hello-363b106aea04a245.d: src/main.rs
+
+src/main.rs:
---
+++ rust_by_example/hello/target/debug/deps/hello-b22db8115be9ccca.d
@@ -0,0 +1,5 @@
+/Users/mike/Developer/rust/learning_rust/rust_by_example/hello/target/debug/deps/hello-b22db8115be9ccca.rmeta: src/main.rs
+
+/Users/mike/Developer/rust/learning_rust/rust_by_example/hello/target/debug/deps/hello-b22db8115be9ccca.d: src/main.rs
+
+src/main.rs:
