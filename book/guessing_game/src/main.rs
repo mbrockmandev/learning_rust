32a6a1d3e4df61c25fb3298bdfc0d24f7d60fbea
Mike Brockman <mbrockmandev@gmail.com>
2024-02-22 12:45:55 -0500

    add basic api setup

--- book/guessing_game/src/main.rs
+++ book/guessing_game/src/main.rs
@@ -2,12 +2,14 @@ use std::io;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess: ");
+
+    println!("Please input your guess!");
 
     let mut guess = String::new();
 
     io::stdin()
         .read_line(&mut guess)
-        .expect("Failed to read line");
-    println!("You guessed: {guess}");
+        .expect("Failed to read line.");
+
+    println!("You guessed: {}", guess);
 }
